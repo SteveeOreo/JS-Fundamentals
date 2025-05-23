25d1ea683cf1644ebd70dc59587265cd4abfe54e
Ekwueme Stephen <stephen.ekwuemec@gmail.com>
2025-05-23 13:10:13 +0100

    created two arguments

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+// const firstArg = process.argv[2];
+// const secondArg = process.argv[3];
+
+// console.log(`${firstArg} is ${secondArg}`);
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + " is " + arg2);
